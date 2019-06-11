ba9e69570f8ab6b106119cc249702a15eb24b0a1
leandrogslva <51672637+leandrogslva@users.noreply.github.com>
2019-06-11 16:02:40 -0300

    Add files via upload

---
+++ src/fruitMixin.js
@@ -0,0 +1,20 @@
+export const fruitMixin = {
+    data() {
+        return {
+            text: 'Hello there!',
+            fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+            filterText: ''
+        }
+    },
+    computed: {
+        filteredFruits() {
+            return this.fruits.filter((element) => {
+                return element.match(this.filterText);
+            });
+            }
+        },
+        created(){
+            console.log('Created');
+        
+    }
+}
